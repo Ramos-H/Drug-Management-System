ec67f3e3f67261ff73436b1b08deff6f9275c19a
Hans Ramos <97885787+Ramos-H@users.noreply.github.com>
2022-12-06 01:03:28 +0800

    Register page now uses AJAX

--- js/script.js
+++ js/script.js
@@ -57,6 +57,71 @@ function requestLogin()
   xhr.send(encodeURIComponent(data));
 }
 
+function requestRegister()
+{
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", '../php/register.php', true);
+
+  //Send the proper header information along with the request
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  xhr.onreadystatechange = () => { // Call a function when the state changes.
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200)
+    {
+      let response = JSON.parse(xhr.responseText);
+      if (response.status === 'SUCCESS')
+      {
+        window.location.href = response.data;
+      }
+      else if (response.status === 'FAILURE')
+      {
+        let usernameField = document.getElementById('username');
+        let passwordField = document.getElementById('password');
+        let confPasswordField = document.getElementById('confirm_password');
+        let usernameFeedback = usernameField.parentElement.getElementsByClassName('invalid-feedback')[0];
+        let passwordFeedback = passwordField.parentElement.getElementsByClassName('invalid-feedback')[0];
+        let confPasswordFeedback = confPasswordField.parentElement.getElementsByClassName('invalid-feedback')[0];
+
+        if (!isNullOrWhitespace(response.data.username))
+        {
+          usernameField.classList.add('is-invalid');
+          usernameFeedback.innerHTML = response.data.username;
+        }
+        else
+        {
+          usernameField.classList.remove('is-invalid');
+          usernameFeedback.innerHTML = '';
+        }
+        
+        if (!isNullOrWhitespace(response.data.password))
+        {
+          passwordField.classList.add('is-invalid');
+          passwordFeedback.innerHTML = response.data.password;
+        }
+        else
+        {
+          passwordField.classList.remove('is-invalid');
+          passwordFeedback.innerHTML = '';
+        }
+        
+        if (!isNullOrWhitespace(response.data.confirm_password))
+        {
+          confPasswordField.classList.add('is-invalid');
+          confPasswordFeedback.innerHTML = response.data.confirm_password;
+        }
+        else
+        {
+          confPasswordField.classList.remove('is-invalid');
+          confPasswordFeedback.innerHTML = '';
+        }
+      }
+    }
+  }
+
+  let data = jsonifyForm(document.forms['registerForm']);
+  xhr.send(encodeURIComponent(data));
+}
+
 function isNullOrWhitespace(str) { return (str == null) || (str.trim().length < 1); }
 
 function jsonifyForm(form)
---
+++ php/register.php
@@ -0,0 +1,103 @@
+<?php
+  require_once 'utils.php';
+  require_once 'constants.php';
+  require_once 'database.php';
+
+  $has_submitted = !empty($_POST);
+  if($has_submitted)
+  {
+    // Decode JSON data from AJAX submission
+    $_POST = json_decode(array_keys($_POST)[0], true);
+  }
+
+  $username = isset($_POST['username']) ? trim($_POST['username']) : null;
+  $password = $_POST['password'] ?? null;
+  $confirm_password = $_POST['confirm_password'] ?? null;
+
+  $has_username = !check_str_empty($username);
+  $has_password = !check_str_empty($password);
+  $has_confirm_password = !check_str_empty($confirm_password);
+
+  $username_too_long = $has_username ? (strlen($username) > MAX_LENGTH_USERNAME) : false;
+
+  // Passwords are not max char limited because having more characters actually help.
+  // See: https://stackoverflow.com/a/98786
+  $password_too_short = $has_password ? (strlen($password) < MIN_LENGTH_PASSWORD) : false;
+
+  $confirm_password_matches = ($has_password && $has_confirm_password) ? ($password === $confirm_password) : false;
+
+  if(false)
+  {
+    echo 'DEBUG MODE: ON<br>';
+    echo sprintf('Has submitted: %s<br>', bool_to_str($has_submitted));
+    echo sprintf('Has username: %s<br>', bool_to_str($has_username));
+    echo sprintf('Has password: %s<br>', bool_to_str($has_password));
+    echo sprintf('Has confirm password: %s<br>', bool_to_str($has_confirm_password));
+    echo '<br>';
+    echo sprintf('Username: %s<br>', ($has_username) ? $username : 'none');
+    echo sprintf('Password: %s<br>', ($has_password) ? $password : 'none');
+    echo sprintf('Confirm Password: %s<br>', ($has_confirm_password) ? $confirm_password : 'none');
+    echo '<br>';
+    echo sprintf('Username too long: %s<br>', bool_to_str($username_too_long));
+    echo sprintf('Password too short: %s<br>', bool_to_str($password_too_short));
+    echo sprintf('Confirm password matches: %s<br>', bool_to_str($confirm_password_matches));
+    echo '<br>';
+  }
+
+  $auth_result = array('status' => '', 'data' => '');
+  // Error reporting
+  $errors = array('username' => '',
+                  'password' => '',
+                  'confirm_password' => '');
+
+  if(!$has_username)
+  {
+    $errors['username'] = 'Please enter a username.';
+    $auth_result['status'] = 'FAILURE';
+  }
+  else if($username_too_long)
+  {
+    $errors['username'] = sprintf('Your username cannot be longer than %d characters. Please remove %d characters to continue.', MAX_LENGTH_USERNAME, strlen($username) - MAX_LENGTH_USERNAME);
+    $auth_result['status'] = 'FAILURE';
+  }
+
+  if(!$has_password)
+  {
+    $errors['password'] = 'Please enter a password.';
+    $auth_result['status'] = 'FAILURE';
+  }
+  else if($password_too_short)
+  {
+    $errors['password'] = sprintf('Your password cannot be shorter than %d characters. Please add at least %d more characters to continue. ', MIN_LENGTH_PASSWORD, MIN_LENGTH_PASSWORD - strlen($password));
+    $auth_result['status'] = 'FAILURE';
+  }
+  
+  if(!$has_confirm_password)
+  {
+    $errors['confirm_password'] = 'Please enter your password again.';
+    $auth_result['status'] = 'FAILURE';
+  }
+  else if(!$confirm_password_matches)
+  {
+    $errors['confirm_password'] = 'Your password and confirm password fields do not match.';
+    $auth_result['status'] = 'FAILURE';
+  }
+
+  if($has_submitted && $has_username && $has_password && $has_confirm_password
+    && !$username_too_long && !$password_too_short && $confirm_password_matches)
+  {
+    insert_new_user($username, $password);
+    $auth_result['status'] = 'SUCCESS';
+  }
+
+  if($auth_result['status'] === 'SUCCESS')
+  {
+    $auth_result['data'] = 'login.html';
+  }
+  else
+  {
+    $auth_result['data'] = $errors;
+  }
+
+  echo json_encode($auth_result);
+?>
